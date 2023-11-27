7e402878db4ddce4a9b1696580be90eb3848b50b
Vilguax <axel.pelassa@hotmail.fr>
2023-11-27 09:39:22 +0100

    init

--- crea_db.py
+++ crea_db.py
@@ -1,100 +1,107 @@
 import sqlite3
 
-conn = sqlite3.connect('kanaps.db')
+conn = sqlite3.connect('dc5-pelassa-axel-api.db')
 c = conn.cursor()
 
 c.execute('''
-    CREATE TABLE IF NOT EXISTS kanaps (
-        _id TEXT PRIMARY KEY,
+    CREATE TABLE IF NOT EXISTS campagnes (
+        id INT PRIMARY KEY,
         name TEXT,
-        price INTEGER,
-        imageUrl TEXT,
         description TEXT,
-        altTxt TEXT,
-        colors TEXT
+        start_date DATE,
+        end_date DATE,
+        budget INT
     )
 ''')
 
 data = [
     {
-    "colors": ["Blue", "White", "Black"],
-    "_id": "107fb5b75607497b96722bda5b504926",
-    "name": "Kanap Sinopé",
-    "price": 1849,
-    "imageUrl": "kanap01.jpeg",
-    "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "altTxt": "Photo d'un canapé bleu, deux places"
-  },
-  {
-    "colors": ["Black/Yellow", "Black/Red"],
-    "_id": "415b7cacb65d43b2b5c1ff70f3393ad1",
-    "name": "Kanap Cyllène",
-    "price": 4499,
-    "imageUrl": "kanap02.jpeg",
-    "description": "Morbi nec erat aliquam, sagittis urna non, laoreet justo. Etiam sit amet interdum diam, at accumsan lectus.",
-    "altTxt": "Photo d'un canapé jaune et noir, quattre places"
-  },
-  {
-    "colors": ["Green", "Red", "Orange"],
-    "_id": "055743915a544fde83cfdfc904935ee7",
-    "name": "Kanap Calycé",
-    "price": 3199,
-    "imageUrl": "kanap03.jpeg",
-    "description": "Pellentesque fermentum arcu venenatis ex sagittis accumsan. Vivamus lacinia fermentum tortor.Mauris imperdiet tellus ante.",
-    "altTxt": "Photo d'un canapé d'angle, vert, trois places"
-  },
-  {
-    "colors": ["Pink", "White"],
-    "_id": "a557292fe5814ea2b15c6ef4bd73ed83",
-    "name": "Kanap Autonoé",
-    "price": 1499,
-    "imageUrl": "kanap04.jpeg",
-    "description": "Donec mattis nisl tortor, nec blandit sapien fermentum at. Proin hendrerit efficitur fringilla. Lorem ipsum dolor sit amet.",
-    "altTxt": "Photo d'un canapé rose, une à deux place"
-  },
-  {
-    "colors": ["Grey", "Purple", "Blue"],
-    "_id": "8906dfda133f4c20a9d0e34f18adcf06",
-    "name": "Kanap Eurydomé",
-    "price": 2249,
-    "imageUrl": "kanap05.jpeg",
-    "description": "Ut laoreet vulputate neque in commodo. Suspendisse maximus quis erat in sagittis. Donec hendrerit purus at congue aliquam.",
-    "altTxt": "Photo d'un canapé gris, trois places"
-  },
-  {
-    "colors": ["Grey", "Navy"],
-    "_id": "77711f0e466b4ddf953f677d30b0efc9",
-    "name": "Kanap Hélicé",
-    "price": 999,
-    "imageUrl": "kanap06.jpeg",
-    "description": "Curabitur vel augue sit amet arcu aliquet interdum. Integer vel quam mi. Morbi nec vehicula mi, sit amet vestibulum.",
-    "altTxt": "Photo d'un canapé gris, deux places"
-  },
-  {
-    "colors": ["Red", "Silver"],
-    "_id": "034707184e8e4eefb46400b5a3774b5f",
-    "name": "Kanap Thyoné",
-    "price": 1999,
-    "imageUrl": "kanap07.jpeg",
-    "description": "EMauris imperdiet tellus ante, sit amet pretium turpis molestie eu. Vestibulum et egestas eros. Vestibulum non lacus orci.",
-    "altTxt": "Photo d'un canapé rouge, deux places"
-  },
-  {
-    "colors": ["Pink", "Brown", "Yellow", "White"],
-    "_id": "a6ec5b49bd164d7fbe10f37b6363f9fb",
-    "name": "Kanap orthosie",
-    "price": 3999,
-    "imageUrl": "kanap08.jpeg",
-    "description": "Mauris molestie laoreet finibus. Aenean scelerisque convallis lacus at dapibus. Morbi imperdiet enim metus rhoncus.",
-    "altTxt": "Photo d'un canapé rose, trois places"
-  }
+      "id": 1,
+      "name": "Campagne de lancement",
+      "description": "Campagne promotionnelle pour le lancement de notre nouveau produit.",
+      "start_date": "2023-01-01",
+      "end_date": "2023-01-31",
+      "budget": 5000
+    },
+    {
+      "id": 2,
+      "name": "Promotion de printemps",
+      "description": "Offres spéciales pour la saison du printemps.",
+      "start_date": "2023-03-01",
+      "end_date": "2023-03-31",
+      "budget": 3000
+    },
+    {
+      "id": 3,
+      "name": "Campagne estivale",
+      "description": "Profitez de l'été avec nos offres exclusives.",
+      "start_date": "2023-06-01",
+      "end_date": "2023-08-31",
+      "budget": 7000
+    },
+    {
+      "id": 4,
+      "name": "Back-to-School Sale",
+      "description": "Préparez-vous pour la rentrée avec nos promotions exceptionnelles.",
+      "start_date": "2023-08-15",
+      "end_date": "2023-09-15",
+      "budget": 4000
+    },
+    {
+      "id": 5,
+      "name": "Promotion d'automne",
+      "description": "Découvrez nos offres spéciales pour la saison automnale.",
+      "start_date": "2023-10-01",
+      "end_date": "2023-11-15",
+      "budget": 6000
+    },
+    {
+      "id": 6,
+      "name": "Holiday Extravaganza",
+      "description": "Célébrez les fêtes avec nos offres festives.",
+      "start_date": "2023-12-01",
+      "end_date": "2023-12-31",
+      "budget": 8000
+    },
+    {
+      "id": 7,
+      "name": "Nouvel An Nouvelles Offres",
+      "description": "Commencez la nouvelle année avec nos offres exclusives.",
+      "start_date": "2024-01-01",
+      "end_date": "2024-01-31",
+      "budget": 5500
+    },
+    {
+      "id": 8,
+      "name": "Love is in the Air",
+      "description": "Offres spéciales pour la saison de la Saint-Valentin.",
+      "start_date": "2024-02-01",
+      "end_date": "2024-02-14",
+      "budget": 2500
+    },
+    {
+      "id": 9,
+      "name": "Spring Clearance",
+      "description": "Économisez gros avec nos articles en liquidation de printemps.",
+      "start_date": "2024-04-01",
+      "end_date": "2024-04-30",
+      "budget": 3500
+    },
+    {
+      "id": 10,
+      "name": "Summer Flash Sale",
+      "description": "Saisissez les offres éclair de l'été avant qu'elles ne disparaissent.",
+      "start_date": "2024-07-01",
+      "end_date": "2024-07-15",
+      "budget": 4500
+    }
 ]
 
 for item in data:
     c.execute('''
-        INSERT INTO kanaps (_id, name, price, imageUrl, description, altTxt, colors)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-    ''', (item['_id'], item['name'], item['price'], item['imageUrl'], item['description'], item['altTxt'], ','.join(item['colors'])))
+        INSERT INTO campagnes (id, name, description, start_date, end_date, budget)
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', (item['id'], item['name'], item['description'], item['start_date'], item['end_date'], item['budget']))
 
 conn.commit()
 conn.close()
