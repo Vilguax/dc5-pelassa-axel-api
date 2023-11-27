a39ca2effbe721c00eeb1abc07dee4a1e09e06db
Vilguax <axel.pelassa@hotmail.fr>
2023-11-27 10:13:18 +0100

    fix acces API

--- route.js
+++ route.js
@@ -1,56 +1,58 @@
-app.get('/api/campaigns', (req, res) => {
-    const sql = "SELECT * FROM campagnes";
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-            res.status(400).json({"erreur ": err.message});
-            return;
-        }
-        res.json({
-            "message": "succes ",
-            "data": rows
+module.exports = function(app, db) {
+    app.get('/api/campaigns', (req, res) => {
+        const sql = "SELECT * FROM campagnes";
+        db.all(sql, [], (err, rows) => {
+            if (err) {
+                res.status(400).json({"erreur ": err.message});
+                return;
+            }
+            res.json({
+                "message": "succes ",
+                "data": rows
+            });
         });
     });
-});
-
-app.get('/api/campaigns/:id', (req, res) => {
-    const sql = "SELECT * FROM campagnes WHERE id = ?";
-    const params = [req.params.id];
-    db.get(sql, params, (err, row) => {
-        if (err) {
-            res.status(400).json({"erreur ": err.message});
-            return;
-        }
-        res.json({
-            "message": "succes ",
-            "data": row
+    
+    app.get('/api/campaigns/:id', (req, res) => {
+        const sql = "SELECT * FROM campagnes WHERE id = ?";
+        const params = [req.params.id];
+        db.get(sql, params, (err, row) => {
+            if (err) {
+                res.status(400).json({"erreur ": err.message});
+                return;
+            }
+            res.json({
+                "message": "succes ",
+                "data": row
+            });
         });
     });
-});
-
-app.post('/api/campaigns', (req, res) => {
-    const { name, description, start_date, end_date, budget } = req.body;
-    const sql = `INSERT INTO campagnes (name, description, start_date, end_date, budget) VALUES (?, ?, ?, ?, ?)`;
-    const params = [name, description, start_date, end_date, budget];
-    db.run(sql, params, function (err) {
-        if (err) {
-            res.status(400).json({"erreur ": err.message});
-            return;
-        }
-        res.json({
-            "message": "succes ",
-            "data": { id: this.lastID }
+    
+    app.post('/api/campaigns', (req, res) => {
+        const { name, description, start_date, end_date, budget } = req.body;
+        const sql = `INSERT INTO campagnes (name, description, start_date, end_date, budget) VALUES (?, ?, ?, ?, ?)`;
+        const params = [name, description, start_date, end_date, budget];
+        db.run(sql, params, function (err) {
+            if (err) {
+                res.status(400).json({"erreur ": err.message});
+                return;
+            }
+            res.json({
+                "message": "succes ",
+                "data": { id: this.lastID }
+            });
         });
     });
-});
-
-app.delete('/api/campaigns/:id', (req, res) => {
-    const sql = 'DELETE FROM campagnes WHERE id = ?';
-    const params = [req.params.id];
-    db.run(sql, params, function (err) {
-        if (err) {
-            res.status(400).json({ "error": res.message });
-            return;
-        }
-        res.json({ "message": "suppression", rows: this.changes });
+    
+    app.delete('/api/campaigns/:id', (req, res) => {
+        const sql = 'DELETE FROM campagnes WHERE id = ?';
+        const params = [req.params.id];
+        db.run(sql, params, function (err) {
+            if (err) {
+                res.status(400).json({ "error": res.message });
+                return;
+            }
+            res.json({ "message": "suppression", rows: this.changes });
+        });
     });
-});
+};
--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+const db = require('./db_gestion');
+const routes = require('./route');
+
 app.use(bodyParser.json());
+
+routes(app, db);
 
 app.listen(port, () => {
     console.log(`Le serveur tourne sur le port : ${port}`);
